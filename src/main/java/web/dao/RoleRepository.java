7fa416ac8ea8db4b4b0a9f3e477ad054833e709c
User <nexx01@mail.ru>
2020-09-13 12:46:40 +0300

    небольшой рефакторинг

---
+++ src/main/java/web/dao/RoleRepository.java
@@ -0,0 +1,9 @@
+package web.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import web.model.Role;
+
+public interface RoleRepository extends CrudRepository<Role,Long> {
+    Role findByRoleName(String roleName);
+
+}
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,16 +1,10 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//import web.dao.UserDao;
 import web.dao.UserRepository;
-import web.model.Role;
 import web.model.User;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
