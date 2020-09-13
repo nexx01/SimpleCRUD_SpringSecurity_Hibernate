674daebb39391a05ab0aca701c65d750e06b0815
User <nexx01@mail.ru>
2020-09-13 12:19:04 +0300

    небольшой рефакторинг

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -1,16 +1,10 @@
 package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-//import web.dao.UserDao;
 import web.dao.UserRepository;
-import web.model.Role;
 import web.model.User;
-
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,7 +15,6 @@ public class UserServiceImpl implements UserService {
     @Transactional
     @Override
     public void saveUser(User user) {
-
         userRepository.save(user);
     }
 
