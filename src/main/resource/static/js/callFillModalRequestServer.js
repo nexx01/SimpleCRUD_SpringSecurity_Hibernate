13d3d03416cc83312ba71b8fd54a4f865a638b0b
monster <nexx01@mail.ru>
2020-09-29 13:22:34 +0300

    Revert "с"
    This reverts commit 9bedf5a5

--- src/main/resource/static/js/callFillModalRequestServer.js
+++ src/main/resource/static/js/callFillModalRequestServer.js
@@ -1,36 +1,25 @@
 
 $('document').ready(function (){
-    $('.table .btn').on('click', function (event) {
+    $('.btn').on('click', function (event) {
         event.preventDefault();
 
+        var href = $(this).attr('href');
 
-        $('#edit-modal').modal('show');
+        $.get(href, function (user, status) {
+            $('#idEdit').val(user.id);
+            $('#firstNameEdit').val(user.firstName);
+            $('#lastNameEdit').val(user.lastName);
+            $('#emailEdit').val(user.email);
+        })
+        $('#edit-modal').modal();
     });
-
-
-
-
-
-
-    $('#btnEditUser').on('click', function (event) {
-        event.preventDefault();
-
-
-        $('#edit-modal').modal('show');
-    });
-
-
-
-
-    $('#edit').on('click', function (event) {
-        event.preventDefault();
-        alert("Work1")
-
-
-        $('#edit-modal').modal('show');
-    });
-
 });
+
+
+
+
+
+
 
 
 
