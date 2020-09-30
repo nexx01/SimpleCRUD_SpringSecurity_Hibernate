82c7f15967045f6a94a00fe09ab918412aa11cbd
monster <nexx01@mail.ru>
2020-09-30 20:47:47 +0300

    заполнение всеми ролями

--- src/main/resource/static/js/fillModal.js
+++ src/main/resource/static/js/fillModal.js
@@ -4,75 +4,96 @@ $(document).on('click', '#btnEditUser', function (event) {
 
     let dropdown = document.getElementById('selectInModal');
     dropdown.length = 0;
-
+    //
     let defaultOption = document.createElement('option');
-    defaultOption.text = 'Choose role/roles';
+    // defaultOption.text = 'Choose role/roles';
+    //
+    // dropdown.add(defaultOption);
+    // dropdown.selectedIndex = 0;
+    let requestUrlgetOne = requestUrl.concat("/").concat($(this).attr('data-id'))
 
-    dropdown.add(defaultOption);
-    dropdown.selectedIndex = 0;
 
     fetch(requestUrlRoles)
         .then(
-            function(response) {
+
+            function (response) {
+
                 if (response.status !== 200) {
                     console.warn('Looks like there was a problem. Status Code: ' +
                         response.status);
                     return;
                 }
+                let allRoles =[];
 
                 // Examine the text in the response
-                response.json().then(function(data) {
+                response.json().then(function (data) {
                     let option;
+
 
                     for (let i = 0; i < data.length; i++) {
                         option = document.createElement('option');
                         option.text = data[i].roleName;
                         option.value = data[i].roleName;
+                        allRoles.push(data[i].roleName)
+
                         dropdown.add(option);
                     }
                 });
+
+                return allRoles;
             }
-        )
-        .catch(function(err) {
-            console.error('Fetch Error -', err);
-        });
-
-    // $('[name = "editModal"]').modal('show');
-    //
-
-    console.log("ID выбранного юзера(и ID строки)"+$(this).attr('data-id'))
-    // $('.modal').modal(); //открываем все классы модаль
-    let requestUrlgetOne=requestUrl.concat("/").concat($(this).attr('data-id'))
-    console.log("Запрос по адресу"+requestUrlgetOne)
-    $(document).ready(function () {
-        sendRequest('GET',requestUrlgetOne)
-            .then(data => {
-                console.log(data);
-                $('#idEdit').val(data.id);
-                $('#firstNameEdit').val(data.firstName);
-                $('#lastNameEdit').val(data.lastName);
-                $('#ageEdit').val(data.age);
-                $('#emailEdit').val(data.email);
-                $('#selectInModal').selectpicker();
+        ).then(function (allRoles) {
+        $(document).ready(function () {
+            sendRequest('GET', requestUrlgetOne)
+                .then(data => {
+                    console.log(data);
+                    $('#idEdit').val(data.id);
+                    $('#firstNameEdit').val(data.firstName);
+                    $('#lastNameEdit').val(data.lastName);
+                    $('#ageEdit').val(data.age);
+                    $('#emailEdit').val(data.email);
+                    $('#selectInModal').selectpicker();
 
 
+                   $.each(allRoles, function (indexRole, nameRole){
+                       $.each(data.roles, function (indexRoleUser, roleUser){
+                           console.log("nameRole "+nameRole+ " roleUser "+ roleUser.roleName)
+                         if(nameRole===roleUser.roleName){
 
 
-                // sendRequest('GET',requestUrlRoles)
-                // .then(roles=>{
-                //     console.log(roles)
-                //     roles.forEach(function (role){
-                //             $("#selectInModal").append(
-                //                 $('<option value="1">New option</option>')
-                //             );
-                //         });
-                //
-                //     });
-            });
+                         }
+                       })
+                   })
 
-        $('#edit-modal').modal();
+
+                    // response.json().then(function (data) {
+                    //     console.log("hbjkh" + data)
+                    // })
+
+                    // sendRequest('GET',requestUrlRoles)
+                    // .then(roles=>{
+                    //     console.log(roles)
+                    //     roles.forEach(function (role){
+                    //             $("#selectInModal").append(
+                    //                 $('<option value="1">New option</option>')
+                    //             );
+                    //         });
+                    //
+                    //     });
+                });
+
+            $('#edit-modal').modal();
+        })
     })
+
+
 })
+
+
+// $('[name = "editModal"]').modal('show');
+//
+
+
 //  .catch(err => console.log(err))
 
 
